ef48b50d8c19fffa253e97c1f837e8028aa4deb4
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-08-11 13:20:51 -0300

    feat: CategoriesRepository in memory and createCategory test

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,27 @@
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoryRepository";
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category | null> {
+    const category = this.categories.find((category) => category.name === name);
+    
+    return category ? category : null;
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories; 
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const newCategory = new Category(); 
+
+    Object.assign(newCategory, {
+      name,
+      description,
+    });
+
+    this.categories.push(newCategory);
+  }
+}
